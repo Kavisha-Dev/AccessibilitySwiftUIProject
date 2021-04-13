982130c266730f9aa9630c9e525a514cd33fb65d
kavisha <kavisha_jg@yahoo.com>
2021-04-13 12:53:10 +0530

    Initial Commit

---
+++ Shared/AccessibilitySwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySwiftUIProjectApp.swift
+//  Shared
+//
+//  Created by Kavisha Sonaal on 13/04/21.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
