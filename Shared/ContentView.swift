f6b951d72ff73cbcb7ff91089fcc33bf2b9d11ab
kavisha <kavisha_jg@yahoo.com>
2021-04-13 12:57:30 +0530

    Added toolbar button

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            
+            Text("Hello")
+                .toolbar(content: {
+                    ToolbarItemGroup(placement: .bottomBar) {
+                        HStack {
+                            Text("").accessibilityHidden(true)
+                            
+                            Button(action: {
+                                //
+                            }) {
+                                Image(systemName: "folder.badge.plus")
+                                    .padding()
+                            }
+                        }
+                        .accessibilityElement()
+                        .accessibilityLabel("Add meal category")
+                        .accessibility(label: Text("Add meal category"))
+                        .accessibility(addTraits: .isButton)
+                    }
+                })
+        }
     }
 }
 
